8c0862f9ce611e89e86f8def546bfd884a40517b
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-02-22 22:54:13 +0000

    I tried using char, but it didn't work

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -6,13 +6,16 @@ namespace VariousTypes
     {
         static void Main()
         {
-            //// Integer variables
+            // Integer variables
             int idade = 42;                         
             uint codigo = 0x126178u;
             ushort codigo2 = 0xBEE;
             ulong codigo3 = 0xABCD4567890UL;
             short neg = -1337;
             long longo = 9876543210L;
+
+            // Char variables
+            char cora = '\u1F338';
 
             // Displaying variable values
             Console.WriteLine(idade);
@@ -22,7 +25,9 @@ namespace VariousTypes
             Console.WriteLine(neg);
             Console.WriteLine(longo);
 
+            // Displaying char values
+            Console.WriteLine(cora);
 
-        }
+        }    
     }
 }
