ce89cded2762e95fa171268257b987bce63f7a4f
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-02-23 22:11:01 +0000

    Add Ask the user to enter a real number

--- Program.cs
+++ Program.cs
@@ -6,11 +6,19 @@ namespace AskForInput
     {
         static void Main(string[] args)
         {
-            // Pedir ao utilizador para inserir um número inteiro.
+            // Ask the user to enter an integer
             Console.WriteLine("Por favor, insira um número inteiro:");
             string enter = Console.ReadLine();
             int num = Convert.ToInt32(enter);
-            Console.WriteLine($"Inseris-te o número inteiro: {num}!");
+            Console.WriteLine($"Inseriste um número inteiro: {num}!");
+
+            // Ask the user to enter a real number
+            Console.Write("Insira um número real: ");
+            string numreal = Console.ReadLine();
+            float.Parse(numreal);
+            Console.WriteLine($"Inseriste um número real: {numreal}!");
+
+
         }
     }
 }
