370246bb8c0731afb1a5cb5ee3b24f1e98cdcecd
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-02-23 21:21:30 +0000

    Add ask the user to enter an integer

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace AskForInput
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Pedir ao utilizador para inserir um número inteiro
+            Console.WriteLine("Por favor, insira um número inteiro:");
+            string enter = Console.ReadLine();
+            int num = Convert.ToInt32(enter);
+            Console.WriteLine($"Inseris-te o número inteiro: {num}!");
         }
     }
 }
