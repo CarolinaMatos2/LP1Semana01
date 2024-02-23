1e39576916c68c0eb0f2a32be973d946c66ba746
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-02-23 16:21:52 +0000

    Variables changed

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -7,26 +7,28 @@ namespace VariousTypes
         static void Main()
         {
             // Integer variables
-            int idade = 42;                         
-            uint codigo = 0x126178u;
-            ushort codigo2 = 0xBEE;
-            ulong codigo3 = 0xABCD4567890UL;
-            short neg = -1337;
-            long longo = 9876543210L;
+            int i = 42;                         
+            uint c = 0x126178u;
+            ushort c2 = 0x65635;
+            ulong c3 = 0x9876543210UL;
+            short neg = -1637;
+            long l = 9876543210L;
 
             // Char variables
             char cora = '\u1F338';
+            char cav = '\u2620';
 
             // Displaying variable values
-            Console.WriteLine(idade);
-            Console.WriteLine(codigo);
-            Console.WriteLine(codigo2);
-            Console.WriteLine(codigo3);
+            Console.WriteLine(i);
+            Console.WriteLine(c);
+            Console.WriteLine(c2);
+            Console.WriteLine(c3);
             Console.WriteLine(neg);
-            Console.WriteLine(longo);
+            Console.WriteLine(l);
 
             // Displaying char values
             Console.WriteLine(cora);
+            Console.WriteLine(cav);
 
         }    
     }
