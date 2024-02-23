9bd35b495d9bb461eb8a91dab001d6edf1fce5f6
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-02-23 17:35:10 +0000

    Add strings2

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Strings
+           string message = "Hi \u26A1";
+           string message2 = "I am a star \u2B50";
+           string message3 = "Happy Day for you \u2666 \u2600";
+           
+           Console.WriteLine(message);
+           Console.WriteLine(message2);
+           Console.WriteLine(message3);
+        }
+    }
+}
